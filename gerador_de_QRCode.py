72d008001a6c37f23943e2971bede3e28ef8a255
Code Key <105808889+CCodeKey@users.noreply.github.com>
2025-03-08 13:28:20 -0300

    script para gerar qrcode

---
+++ gerador_de_QRCode.py
@@ -0,0 +1,6 @@
+import segno
+seuLink = "https://github.com/CCodeKey/IFit"
+corDoQR = "#191919"
+corDoFundo = "#f4a100"
+img = segno.make(seuLink)
+img.save('img.png', dark=corDoQR , light=corDoFundo, scale=5)
